77d441ae2d73c4f69d66ab4eeed0f6680b3954e0
yangkunxiao <15738518697@163.com>
2020-01-29 16:35:50 +0800

    codeSplitting

--- demo05/src/index.js
+++ demo05/src/index.js
@@ -1,18 +1,18 @@
 import '../assets/css/index.css';
 
-// var btn = document.createElement('button');
-// btn.innerHTML = '按钮';
+var btn = document.createElement('button');
+btn.innerHTML = '按钮';
 
-// document.body.appendChild(btn);
+document.body.appendChild(btn);
 
-// btn.addEventListener('click', function() {
-//     var div = document.createElement('div');
-//     div.innerHTML = 'item';
-//     document.body.appendChild(div);
-// })
+btn.addEventListener('click', function() {
+    var div = document.createElement('div');
+    div.innerHTML = 'item';
+    document.body.appendChild(div);
+})
 
-import counter from '../assets/javascript/counter.js';
-import number from '../assets/javascript/number.js';
+// import counter from '../assets/javascript/counter.js';
+// import number from '../assets/javascript/number.js';
 
-counter();
-number()
+// counter();
+// number()
--- demo10/src/index.js
+++ demo10/src/index.js
@@ -1,24 +1,24 @@
-import { add } from './math';
+// import { add } from './math';
 
-let sum = add(1,4);
-console.log(sum)
+// let sum = add(1,4);
+// console.log(sum)
 
 
 //异步导入
-// function getComponent(){
-//     return import(/* webpackChunkName:"lodash" */ "lodash").then(({ default:_ }) => {
-//         let div = document.createElement('div');
-//         div.innerHTML = _.join(['hello','world','lodash']);
-//         return div;
-//     })
-// };
+function getComponent(){
+    return import(/* webpackChunkName:"lodash" */ "lodash").then(({ default:_ }) => {
+        let div = document.createElement('div');
+        div.innerHTML = _.join(['hello','world','lodash']);
+        return div;
+    })
+};
 
-// getComponent().then((ele) => {
-//     document.body.appendChild(ele)
-// })
+getComponent().then((ele) => {
+    document.body.appendChild(ele)
+})
 
-import _ from 'lodash';
-import $ from 'jquery';
-let div = document.createElement('div');
-div.innerHTML = _.join(['hello', 'world', 'lodash']);
-document.body.appendChild(div)
+// import _ from 'lodash';
+// import $ from 'jquery';
+// let div = document.createElement('div');
+// div.innerHTML = _.join(['hello', 'world', 'lodash']);
+// document.body.appendChild(div)
